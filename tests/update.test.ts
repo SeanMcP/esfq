a5e6514fe506adc993be90f0def100b58cf0d215
Sean McPherson <git@seanmcp.com>
2021-02-18 07:00:15 -0500

    feat: add test for data mutation in update

--- tests/update.test.ts
+++ tests/update.test.ts
@@ -14,6 +14,11 @@ const data: Item[] = [
   { date: "2021-01-13T18:35:23.675Z", id: 2, name: "almonds", value: 10 },
 ];
 
+test('updates does not mutate data', () => {
+  const { result } = update<Item>(data).set(item => ({ ...item, name: 'cashews' })).where(() => true)
+  assert.not.equal(data, result)
+})
+
 test('updates data (set first)', () => {
   const { result } = update<Item>(data).set(item => ({ ...item, name: 'cashews' })).where(item => item.id === 2)
   assert.equal(result.map(item => item.name), ['peanuts', 'cashews'])
